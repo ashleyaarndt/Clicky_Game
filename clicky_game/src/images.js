bd38a33f7ce7bb37796c0a1502aa5f5eef34dd2f
ashley <ashleyaarndt@outlook.com>
2019-06-02 19:11:04 -0400

    final

---
+++ clicky_game/src/images.js
@@ -0,0 +1,29 @@
+import barney from './Images/barney.png';
+import ted from './Images/ted.png';
+import marshall from './Images/marshall.png';
+import lily from './Images/lily.png';
+import mother from './Images/mother.png';
+import robin from './Images/robin.png';
+import horn  from './Images/horn.png';
+import umbrella from './Images/umbrella.png';
+import driver from './Images/driver.png';
+import wendy from './Images/wendy.png';
+import carl from './Images/carl.png';
+import randy from './Images/randy.png';
+
+const images = [
+    barney,
+    ted,
+    marshall,
+    lily,
+    mother,
+    robin,
+    horn,
+    umbrella,
+    driver,
+    wendy,
+    carl,
+    randy
+];
+
+export default images;
