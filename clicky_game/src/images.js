53d0b5c9ed677c182726d3b89f64ab46d578ed57
ashley <ashleyaarndt@outlook.com>
2019-06-04 19:00:21 -0400

    Tweaks

--- clicky_game/src/images.js
+++ clicky_game/src/images.js
@@ -1,29 +1,31 @@
-import barney from './Images/barney.png';
-import ted from './Images/ted.png';
-import marshall from './Images/marshall.png';
-import lily from './Images/lily.png';
-import mother from './Images/mother.png';
-import robin from './Images/robin.png';
-import horn  from './Images/horn.png';
-import umbrella from './Images/umbrella.png';
-import driver from './Images/driver.png';
-import wendy from './Images/wendy.png';
-import carl from './Images/carl.png';
-import randy from './Images/randy.png';
+import barney from "./Images/barney.png";
+import BG from "./Images/BG.png";
+import ted from "./Images/ted.png";
+import marshall from "./Images/marshall.png";
+import lily from "./Images/lily.png";
+import mother from "./Images/mother.png";
+import robin from "./Images/robin.png";
+import horn from "./Images/horn.png";
+import umbrella from "./Images/umbrella.png";
+import driver from "./Images/driver.png";
+import wendy from "./Images/wendy.png";
+import carl from "./Images/carl.png";
+import randy from "./Images/randy.png";
 
 const images = [
-    barney,
-    ted,
-    marshall,
-    lily,
-    mother,
-    robin,
-    horn,
-    umbrella,
-    driver,
-    wendy,
-    carl,
-    randy
+  barney,
+  BG,
+  ted,
+  marshall,
+  lily,
+  mother,
+  robin,
+  horn,
+  umbrella,
+  driver,
+  wendy,
+  carl,
+  randy
 ];
 
 export default images;
